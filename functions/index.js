017878918611f60635b949a552365075a300f36f
Lukas Seyfarth <lukas.seyfarth@blu-beyond.com>
2020-02-15 21:10:39 +0100

    kinda works

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,67 @@
 const functions = require('firebase-functions');
 
-const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.onFileChange = functions.storage.object().onFinalize(event => {
-    const bucket = event.bucket
-    const contentType = event.contentType
-    const filePath = event.name
-    console.log(contentType + " on file: " + bucket);
- console.log("changing file");
- return
+const os = require('os');
+const path = require('path');
+const cors = require('cors')({ origin: true })
+const Busboy = require('busboy')
+const fs = require("fs");
+const {Storage} = require('@google-cloud/storage');
+
+const storage = new Storage({
+    pojectId: 'nebenkosten-6b4ec',
+    keyFilename: "nebenkosten-6b4ec-firebase-adminsdk-4hxit-b7aefb12d5.json"
 });
 
-exports.onFileDelete = functions.storage.object().onDelete(event => {
-    console.log("deleting file");
-    return;
+// const gcconfig = {
+//     pojectId: 'nebenkosten-6b4ec',
+//     keyFilename: "nebenkosten-6b4ec-firebase-adminsdk-4hxit-b7aefb12d5.json"
+// };
+
+// const storage = require('@google-cloud/storage')(gcconfig);
+
+
+
+exports.uploadFile = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+      if (req.method !== "POST") {
+        return res.status(500).json({
+          message: "Not allowed"
+        });
+      }
+      const busboy = new Busboy({ headers: req.headers });
+      let uploadData = null;
+
+      busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+        const filepath = path.join(os.tmpdir(), filename);
+        uploadData = { file: filepath, type: mimetype };
+        file.pipe(fs.createWriteStream(filepath));
+      });
+
+      busboy.on("finish", () => {
+        const bucket = storage.bucket("nebenkosten-6b4ec.appspot.com");
+        bucket
+          .upload(uploadData.file, {
+            uploadType: "media",
+            metadata: {
+              metadata: {
+                contentType: uploadData.type
+              }
+            }
+          })
+          .then(() => {
+            res.status(200).json({
+              message: "It worked!"
+
+            });
+            return null;
+          })
+          .catch(err => {
+            res.status(500).json({
+              error: err
+            });
+          });
+      });
+      busboy.end(req.rawBody);
+    });
   });
